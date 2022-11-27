fc8f7fbd19cef4c191c956cb9aa8e996e0ef0534
Tín Chín <77972801+lhbtin9@users.noreply.github.com>
2022-11-27 17:19:37 +0700

    27/11

--- common/js/main.js
+++ common/js/main.js
@@ -1,10 +1,41 @@
+// ##### Header Slider #####
 $(document).ready(function () {
     $('.header__silder-owl').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
+        loop: true,
+        margin: 0,
+        nav: true,
         items: 1,
         dots: false,
-        navText: [`<i class="fa-regular fa-chevron-left"></i>`,`<i class="fa-regular fa-chevron-right"></i>`]
+        navText: [`<i class="fa-regular fa-chevron-left"></i>`, `<i class="fa-regular fa-chevron-right"></i>`]
     })
 });
+
+// #### Show/hide Modal #####
+$(document).ready(function () {
+    const modalOpenBtn = $('#header__location-panel');
+    const modalCloseBtn = $('#modal__header-close-btn');
+    const modalElement = $('#modal');
+    $(modalOpenBtn).click(function (e) { 
+        e.preventDefault();
+        $(modalElement).addClass('modal--active');
+    });
+    $(modalCloseBtn).click(function (e) { 
+        e.preventDefault();
+        $(modalElement).removeClass('modal--active');
+    });
+});
+
+// ##### Modal input filter items #####
+const modalLocationInput = document.getElementById('modal-filter-locaion');
+modalLocationInput.onkeyup = function () {
+    var modalFilter = modalLocationInput.value.toUpperCase();
+    var lis = document.getElementsByClassName("modal__location-item");
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].innerHTML;
+        
+        if (name.toUpperCase().indexOf(modalFilter) == 0)
+            lis[i].style.display = 'inline';
+        else
+            lis[i].style.display = 'none';
+    }
+}
